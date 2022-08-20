54595b6fe26c563485e64b1dbe1093799e8e4526
bilinkis <nico@bilinkis.com>
2022-08-19 22:30:31 -0500

    feat: first block suscription implementation

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,46 @@
 import Head from 'next/head'
+import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+import React from "React";
+const web3 = createAlchemyWeb3("wss://eth-goerli.g.alchemy.com/v2/0jX8azzT1Ed6Xhg7Ead6VOloiCTbn5m6");
 
-export default function Home() {
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      blocks:[]
+    }
+
+  }
+  componentDidMount = () =>{
+    this.checkBlocks();
+  }
+  checkBlocks = () => {
+    web3.eth.subscribe("newBlockHeaders",(error,event)=>{
+      console.log(this.state.blocks)
+      let newBlockObject = this.state.blocks;
+      newBlockObject.push(event);
+      this.setState({blocks:newBlockObject});
+    })
+  }
+  render(){
+
+  
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title></title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1>Latest blocks</h1>
+        {this.state.blocks.map((block)=>{
+          return(<h1 key={block.number}>{block.number}</h1>)
+        })}
+        
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -207,3 +190,6 @@ export default function Home() {
     </div>
   )
 }
+}
+
+
